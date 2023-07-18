cbce07a73a1945f9b12397b1dce5fd75cce06677
clockiz <clockiz@example.com>
2023-07-18 12:44:08 +0200

    now flag is displayed, when following the route of a countryCode

--- src/Pages/CountryDetail.js
+++ src/Pages/CountryDetail.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { getCountryDetail } from "../Services";
+import './CountryDetail.css';
 export default function CountryDetail(props) {
+    const {countryCode} = useParams();
+    const[detail, setDetail] = useState({});
+
+    useEffect(() => {
+        getCountryDetail(countryCode).then(result => {
+            console.log("result.data", result.data);
+            setDetail(result.data);
+        }, [countryCode]);
+
+    }, [countryCode])
+    console.log("countryCode: ", countryCode);
     return (
-        <div>
-            Details of the country
+        <div className="country-detail-wrapper">
+            <div>
+                <img src={detail.flags?.png} alt={detail.name}/>
+            </div>
+            <div></div>
+
         </div>
     )
 }
